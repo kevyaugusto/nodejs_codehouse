ddff5dfa77fe8348ccd2593d0e452fd01400f33f
Kevy Augusto <kevyaugusto@gmail.com>
2017-05-25 21:05:44 +1000

    Implemented middlewares to handle errors 404 and 500 in the application.

--- config/express.js
+++ config/express.js
@@ -8,21 +8,41 @@ module.exports = function () {
 
 	var app = express();
 
+	//middlewares before request arrive into routes
+	/*
+		every function below are added to the pipeline of the express to be executed
+		before execute the route itself configured
+	*/
+
 	app.use(express.static('./app/public')); //allow files (CSS, JS, Imgs, etc) to be read in express when requesting the page
 	app.set('view engine', 'ejs'); //ejs = embeddedjs
 	app.set('views', './app/views'); //set the path where views will be placed
-
-	//middlewares before request arrive into routes
+	
 	app.use(bodyParser.urlencoded(
 		{extended:true} //accepts complex JSON structures {name: 'Book1', price:5, author:{name:Caxias}}
 	));
 	app.use(bodyParser.json());
 	app.use(expressValidator());
-	//end middlewares
 
 	expressLoad('routes', {cwd: 'app'}) //find only inside the path app, to avoid scan in the whole project
 		.then('infrastructure')
 		.into(app);
+
+	//treating errors should be after executing expressLoad('routes')
+	app.use(function(request, response, nextFunction){
+		response.status(404).render("errors/404");
+		nextFunction();
+	});	
+
+	app.use(function(error, request, response, nextFunction){
+		if(process.env.NODE_ENV == "production"){
+			response.status(500).render("errors/500");
+			return;
+		}		
+		nextFunction(error);
+	});	
+
+	//end middlewares
 
 	return app;
 }
